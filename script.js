e0ce00c38d208fbf28c8ed6b6221793489dd4a5a
Steven Rogers <steven.rogers1990@gmail.com>
2017-03-07 21:54:17 +0000

    further functionality added

--- script.js
+++ script.js
@@ -2,22 +2,19 @@ $(document).ready(function(){
 
 	var count = parseInt($("#num").html());
 	var time = parseInt($("#breakNum").html());
-	console.log(count);
 
-	$("#reset").hide();
+	$("#stop").hide();
 
 	$("#minus5Clock").click(function(){
 		if(count>5){
 			count -=5;
 			$("#num").html(count);
-			console.log(count);
 		}
 	});
 
 	$("#add5Clock").click(function(){
 		count +=5;
 		$("#num").html(count);
-		console.log(count);
 	});
 
 	$("#minus5Break").click(function(){
@@ -31,22 +28,50 @@ $(document).ready(function(){
 	$("#add5Break").click(function(){
 			time +=5;
 			$("#breakNum").html(time);
-			console.log(time);
 	});
 
 	$("#start").click(function(){
-			var counter = setInterval(timer, 1000);
+
+		var counter = setInterval(timer, 1000);
 
 			function timer(){
-				$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2").hide();
+				$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #reset").hide();
+				$("#stop").show();
 				count -=1;
 				if(count===0){
+					var startBreak = setInterval(breakTimer, 1000);
 					clearInterval(counter);
-					$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2").show();
 				}
 
 				$("#num").html(count);
+
+				function breakTimer(){
+					time -=1;
+					if(time===0){
+						clearInterval(startBreak);
+						$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #reset").show();
+						$("#stop").hide();
+					}
+				$("#breakNum").html(time);	
+				}
 			}
+			$("#stop").click(function(){
+				clearInterval(counter);
+				clearInterval(time);
+				count = 25;
+				time = 5;
+				$("#num").html(25);
+				$("#breakNum").html(5);
+				$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #reset").show();
+				$("#stop").hide();
+			});
+	});
+
+	$("#reset").click(function(){
+		count = 25;
+		time = 5;
+		$("#num").html(25);
+		$("#breakNum").html(5);
 	});
 
 });
